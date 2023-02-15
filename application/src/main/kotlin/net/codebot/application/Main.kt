1cf7342487e797f007b328a4452b7ac6998cab39
Jerry <zy37li@uwaterloo.ca>
2023-02-15 13:28:48 -0800

    initial commit

--- application/src/main/kotlin/net/codebot/application/Main.kt
+++ application/src/main/kotlin/net/codebot/application/Main.kt
@@ -2,19 +2,31 @@ package net.codebot.application
 
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.control.*
 import javafx.scene.control.Label
+import javafx.scene.control.ToolBar
+import javafx.scene.layout.BorderPane
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 import net.codebot.shared.SysInfo
 
 class Main : Application() {
     override fun start(stage: Stage) {
+        val toolBar = ToolBar(
+            Button("New"),
+            Button("Open"),
+            Button("Save")
+        )
+        val border = BorderPane()
+        border.top = toolBar
+
         stage.scene = Scene(
-            StackPane(Label("Hello ${SysInfo.userName}")),
+            border,//StackPane(Label("Hello ${SysInfo.userName}")),
             250.0,
             150.0)
         stage.isResizable = false
         val note1 = Notes()
+
         stage.title = note1.getLastModifiedDate() // Displaying the current date for now.
 
         stage.show()
---
+++ application/src/main/kotlin/net/codebot/application/NoteView.kt
@@ -0,0 +1,19 @@
+package net.codebot.application
+
+import javafx.scene.layout.VBox
+
+/*
+class NoteView (private val model: Model) : VBox(), IView{
+    override fun updateView() {
+        TODO("Not yet implemented")
+    }
+
+    init {
+        this.
+        //somehow get items from model.data
+        data.setOnKeyPressed {
+            if key == spacebar
+        }
+    }
+}
+*/

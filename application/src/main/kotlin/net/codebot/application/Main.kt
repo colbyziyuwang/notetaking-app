891fe85814dfe255537e0339e34f73625ed08ce6
c684wang@uwaterloo.ca <c684wang@uwaterloo.ca>
2023-02-16 21:23:49 -0500

    some small bug

--- application/src/main/kotlin/net/codebot/application/Main.kt
+++ application/src/main/kotlin/net/codebot/application/Main.kt
@@ -5,14 +5,14 @@ import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class Main : Application() {
-    override fun start(stage: Stage) {
+    override fun start(stage: Stage?) {
         // MVC design based off of
         // https://git.uwaterloo.ca/cs349/public/sample-code/-/blob/master/MVC/03.MVC2/src/main/kotlin/MVC2.kt
 
         val model = Model()
         val currentView = CurrentView(model)
-        stage.scene = Scene(currentView.curView, 500.0, 350.0)
-        stage.title = "NoteTaking application"
-        stage.show()
+        stage?.scene = Scene(currentView.curView, 500.0, 350.0)
+        stage?.title = "NoteTaking application"
+        stage?.show()
     }
 }

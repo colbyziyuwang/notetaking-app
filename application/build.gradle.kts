d23f36d4dbae453279963f7e87266a0eaae0490c
Colby Wang <c684wang@uwaterloo.ca>
2023-03-03 17:23:18 -0500

    Update build.gradle.kts

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("org.xerial:sqlite-jdbc:3.39.3.0")
+    implementation("org.jetbrains.exposed", "exposed-core", "0.40.1")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.40.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", “0.40.1”)
 }
 
 tasks.test {

7e3284bb457644249f3de83e1dd1a07627c35253
aichagh <aichagh2002@gmail.com>
2023-03-22 20:12:23 -0400

    preparing for rich text support

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.jetbrains.exposed", "exposed-core", "0.40.1")
     implementation("org.jetbrains.exposed", "exposed-dao", "0.40.1")
     implementation("org.jetbrains.exposed", "exposed-jdbc", "0.40.1")
+    implementation("org.fxmisc.richtext", "richtextfx", "0.11.0")
 }
 
 tasks.test {

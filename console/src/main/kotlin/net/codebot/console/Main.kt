1e4d528f72adcc373ee1d69b2e3dc4fa92b228f0
Hassan Hashmi <hashmihassan47@gmail.com>
2023-03-23 14:19:03 -0400

    all example code added, time to set up server before configuring to our project

--- console/src/main/kotlin/net/codebot/console/Main.kt
+++ console/src/main/kotlin/net/codebot/console/Main.kt
@@ -1,9 +1,59 @@
 package net.codebot.console
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import net.codebot.shared.SysInfo
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+
+
+val SERVER_ADDRESS = "http://127.0.0.1:8080/messages"
+
+@Serializable
+data class Message(val id: String?, val text: String)
+
 
 fun main() {
     println("Console Application:")
+//    val post_response = post(Message("100", "Hello ${SysInfo.hostname}"))
+//
+//    val get_response = get()
+//    println(get_response) // list of JSON objects from database
     println("Hello ${SysInfo.userName}")
 
 }
+
+fun get(): String {
+    val client = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .followRedirects(HttpClient.Redirect.NEVER)
+        .connectTimeout(Duration.ofSeconds(20))
+        .build()
+
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create(SERVER_ADDRESS))
+        .GET()
+        .build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
+}
+
+fun post(message: Message): String {
+    val string = Json.encodeToString(message)
+
+    val client = HttpClient.newBuilder().build();
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create(SERVER_ADDRESS))
+        .header("Content-Type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(string))
+        .build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    return response.body()
+}
+
